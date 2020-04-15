e801dc8bec8e614fcd7c3815c2724f82239c3048
Ahmad <ahmadmm9610@gmail.com>
2020-04-15 13:12:54 -0700

    Added a Project having 2 beans

---
+++ Task/src/com/revature/beans/Career.java
@@ -0,0 +1,30 @@
+package com.revature.beans;
+
+public class Career {
+	private int GamesPlayed,GoalsScored,TrophiesWon;
+
+	public int getGamesPlayed() {
+		return GamesPlayed;
+	}
+
+	public void setGamesPlayed(int gamesPlayed) {
+		GamesPlayed = gamesPlayed;
+	}
+
+	public int getGoalsScored() {
+		return GoalsScored;
+	}
+
+	public void setGoalsScored(int goalsScored) {
+		GoalsScored = goalsScored;
+	}
+
+	public int getTrophiesWon() {
+		return TrophiesWon;
+	}
+
+	public void setTrophiesWon(int trophiesWon) {
+		TrophiesWon = trophiesWon;
+	}  
+	
+}
---
+++ Task/src/com/revature1/beans/Player.java
@@ -0,0 +1,25 @@
+package com.revature1.beans;
+
+public class Player {
+	private int ShirtNumber;  
+	private String PlayerName,TeamName;
+	public int getShirtNumber() {
+		return ShirtNumber;
+	}
+	public void setShirtNumber(int shirtNumber) {
+		ShirtNumber = shirtNumber;
+	}
+	public String getPlayerName() {
+		return PlayerName;
+	}
+	public void setPlayerName(String playerName) {
+		PlayerName = playerName;
+	}
+	public String getTeamName() {
+		return TeamName;
+	}
+	public void setTeamName(String teamName) {
+		TeamName = teamName;
+	}
+	
+}
